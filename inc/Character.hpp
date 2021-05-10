36e6c51fa2209bc9befdadde617bc611e3733f2d
Snow <a.dolin125@gmail.com>
2021-05-11 00:23:06 +0200

    Update

--- inc/Character.hpp
+++ inc/Character.hpp
@@ -1,1 +1,36 @@
 #pragma once
+#include <iostream>
+#include <map>
+#include <unordered_map>
+#include <vector>
+
+enum class SkillType : uint8_t {
+    Unknown = 0,
+    FireBall = 1,
+    Hurricane = 2,
+    WindStrike = 3,
+    Hudroblast = 4
+};
+
+SkillType SkillTypeCast (int skillNum);
+
+int queueThrow();
+
+
+
+// Character - базовый класс
+class Character {
+public:
+    Character(std::string name, int hp);
+    void setActionType(int number, int commandType);
+
+    virtual ~Character() = default;
+
+
+private:
+    std::string m_name;
+    int m_hp;
+    //std::map<>
+    std::unordered_map<int, std::shared_ptr<Character>> map;
+
+};
